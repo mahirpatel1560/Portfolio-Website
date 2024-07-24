91ca5e996e3d49552349d39740c06428c4425afb
Abdul Nofal <56323614+ARMN88@users.noreply.github.com>
2024-07-24 17:44:05 -0400

    Added THREEJS

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
-const project = document.querySelector(".project");
-const projectImg = document.querySelector(".projectImg");
-const projectContent = document.querySelector(".projectContent");
+import * as THREE from "three";
+
+const size = window.innerWidth * 0.4;
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color("#111111");
+const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(size, size);
+const mainImg = document.querySelector(".mainImg");
+mainImg.appendChild(renderer.domElement);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+scene.add(ambientLight);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+directionalLight.position.set(200, 500, 300);
+scene.add(directionalLight);
+
+const group = new THREE.Group();
+
+// Material //
+const material = new THREE.MeshStandardMaterial({
+  color: "rgb(160, 53, 182)",
+});
+
+const geometry = new THREE.TorusKnotGeometry(10, 3, 200, 32);
+const torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
+
+camera.position.z = 40;
+
+function animate() {
+  torus.rotation.x += 0.003;
+  torus.rotation.y += 0.003;
+  renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate);
