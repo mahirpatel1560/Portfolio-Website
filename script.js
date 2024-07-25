a1b5c35539f003109a6c48a53ddcbaea8529bc38
Abdul Nofal <56323614+ARMN88@users.noreply.github.com>
2024-07-25 16:26:29 -0400

    Added Post Processing

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 import * as THREE from "three";
+import { EffectComposer } from "EffectComposer";
+import { RenderPass } from "RenderPass";
+import { UnrealBloomPass } from "UnrealBloomPass";
 
 const size = window.innerWidth * 0.4;
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color("#111111");
 const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(size, size);
 const mainImg = document.querySelector(".mainImg");
 mainImg.appendChild(renderer.domElement);
