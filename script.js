49132d269d36031cf17a643d0a140e297257221e
Abdul Nofal <56323614+ARMN88@users.noreply.github.com>
2024-07-27 15:04:54 -0400

    3D Interactivity

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 import * as THREE from "three";
-import splinetoolloader from "https://cdn.jsdelivr.net/npm/@splinetool/loader@1.9.3/+esm";
+import { GLTFLoader } from "gltf";
 
 const size = window.innerWidth * 0.4;
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+
+const mouse = {
+  x: 0,
+  y: 0,
+};
 
 const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(size, size);
@@ -17,133 +22,49 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
 directionalLight.position.set(200, 500, 300);
 scene.add(directionalLight);
 
-const group = new THREE.Group();
-
 // Material //
 const material = new THREE.MeshStandardMaterial({
   color: "#ff2b2b",
 });
 
-const geometry = new THREE.TorusKnotGeometry(10, 3, 200, 32);
-const torus = new THREE.Mesh(geometry, material);
-scene.add(torus);
+const loader = new GLTFLoader();
 
-camera.position.z = 40;
+let model = undefined;
+
+loader.load(
+  "models/icon.glb",
+  function (gltf) {
+    model = gltf.scene;
+    model.children[0].material = material;
+    scene.add(model);
+  },
+  // called while loading is progressing
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  // called when loading has errors
+  function (error) {
+    console.log("An error happened");
+  }
+);
+
+camera.position.z = 14;
+camera.position.y = 1;
 
 function animate() {
-  torus.rotation.x += 0.003;
-  torus.rotation.y += 0.003;
-  renderer.render(scene, camera);
-}
-renderer.setAnimationLoop(animate);
-/*
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import SplineLoader from '@splinetool/loader';
-
-// camera
-const camera = new THREE.OrthographicCamera(window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2,  -50000, 100000);
-camera.position.set(565.14, 375.08, 723.2);
-camera.quaternion.setFromEuler(new THREE.Euler(-0.52, 0.6, 0.31));
-
-// scene
-const scene = new THREE.Scene();
-
-// spline scene
-const loader = new SplineLoader();
-loader.load(
-  'https://prod.spline.design/j2JC3Q-OvhmaMsLU/scene.splinecode',
-  (splineScene) => {
-    scene.add(splineScene);
+  if (model) {
+    model.rotation.y = linear(model.rotation.y, (mouse.x - 0.7) / 4, 0.1);
+    model.rotation.x = linear(model.rotation.x, (mouse.y - 0.9) / 4, 0.1);
   }
-);
-
-// renderer
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animate);
-document.body.appendChild(renderer.domElement);
-
-// scene settings
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFShadowMap;
-
-scene.background = new THREE.Color('#663d26');
-renderer.setClearAlpha(1);
-
-// orbit controls
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.125;
-
-window.addEventListener('resize', onWindowResize);
-function onWindowResize() {
-  camera.left = window.innerWidth / - 2;
-  camera.right = window.innerWidth / 2;
-  camera.top = window.innerHeight / 2;
-  camera.bottom = window.innerHeight / - 2;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-function animate(time) {
-  controls.update();
   renderer.render(scene, camera);
 }
-*/
-
-/*
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import SplineLoader from '@splinetool/loader';
-
-// camera
-const camera = new THREE.OrthographicCamera(window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2,  -50000, 10000);
-camera.position.set(0, 0, 0);
-camera.quaternion.setFromEuler(new THREE.Euler(0, 0, 0));
-
-// scene
-const scene = new THREE.Scene();
-
-// spline scene
-const loader = new SplineLoader();
-loader.load(
-  'https://prod.spline.design/tug4bdkwuqKzDEcO/scene.splinecode',
-  (splineScene) => {
-    scene.add(splineScene);
-  }
-);
-
-// renderer
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
-document.body.appendChild(renderer.domElement);
 
-// scene settings
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFShadowMap;
+window.onmousemove = (e) => {
+  mouse.x = e.clientX / innerWidth;
+  mouse.y = e.clientY / innerHeight;
+};
 
-scene.background = new THREE.Color('#a8a8a8');
-renderer.setClearAlpha(1);
-
-// orbit controls
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.125;
-
-window.addEventListener('resize', onWindowResize);
-function onWindowResize() {
-  camera.left = window.innerWidth / - 2;
-  camera.right = window.innerWidth / 2;
-  camera.top = window.innerHeight / 2;
-  camera.bottom = window.innerHeight / - 2;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+function linear(start, end, increment) {
+  return start + (end - start) * increment;
 }
-
-function animate(time) {
-  controls.update();
-  renderer.render(scene, camera);
-}
-*/
