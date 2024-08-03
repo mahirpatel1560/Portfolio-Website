65fae69a75491bee81eac8589d1edb98ac44aa90
Abdul Nofal <56323614+ARMN88@users.noreply.github.com>
2024-08-03 08:16:04 -0400

    Added Achievements

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 import * as THREE from "three";
-import { GLTFLoader } from "gltf";
 
 const size = window.innerWidth * 0.4;
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
-
-const mouse = {
-  x: 0,
-  y: 0,
-};
 
 const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(size, size);
@@ -22,49 +16,22 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
 directionalLight.position.set(200, 500, 300);
 scene.add(directionalLight);
 
+const group = new THREE.Group();
+
 // Material //
 const material = new THREE.MeshStandardMaterial({
-  color: "#ff2b2b",
+  color: "#ff1c1c",
 });
 
-const loader = new GLTFLoader();
+const geometry = new THREE.TorusKnotGeometry(10, 3, 200, 32);
+const torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
 
-let model = undefined;
-
-loader.load(
-  "models/icon.glb",
-  function (gltf) {
-    model = gltf.scene;
-    model.children[0].material = material;
-    scene.add(model);
-  },
-  // called while loading is progressing
-  function (xhr) {
-    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-  },
-  // called when loading has errors
-  function (error) {
-    console.log("An error happened");
-  }
-);
-
-camera.position.z = 14;
-camera.position.y = 1;
+camera.position.z = 40;
 
 function animate() {
-  if (model) {
-    model.rotation.y = linear(model.rotation.y, (mouse.x - 0.7) / 4, 0.1);
-    model.rotation.x = linear(model.rotation.x, (mouse.y - 0.9) / 4, 0.1);
-  }
+  torus.rotation.x += 0.003;
+  torus.rotation.y += 0.003;
   renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
-
-window.onmousemove = (e) => {
-  mouse.x = e.clientX / innerWidth;
-  mouse.y = e.clientY / innerHeight;
-};
-
-function linear(start, end, increment) {
-  return start + (end - start) * increment;
-}
